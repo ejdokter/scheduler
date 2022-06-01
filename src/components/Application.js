7f03118f5f622c3be537fba863eb4fd6a5b40eef
ejdokter <edokter11@gmail.com>
2022-06-01 14:58:21 -0600

    import axios and useEffect and make get for days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios"
 
 import "components/Application.scss";
 import DayList from "components/DayList.js"
@@ -43,27 +44,21 @@ const appointments = {
   }
 };
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
 
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday")
+  const [day, setDay] = useState([])
+  const [days, setDayData] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:8001/api/days')
+    .then(response => {
+      console.log(response)
+      setDayData(response.data)
+    })
+    .catch(err => console.log(err))
+  }, [])
 
   return (
     <main className="layout">
